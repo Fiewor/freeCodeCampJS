fb36d8eb2d0c95179c6add35cebb4dc31e45cece
Fiewor <johnfiewor@gmail.com>
2023-09-21 18:16:01 +0100

    add yesterday's daily solution

---
+++ leetcode/minOperations.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/description/
+
+def minOperations(nums: list[int], x: int) -> int:
+    target, n = sum(nums) - x, len(nums)
+
+    if target == 0:
+        return n
+
+    left = cur_sum = max_len = 0
+
+    for right, val in enumerate(nums):
+        cur_sum += val
+
+        while left <= right and cur_sum > target:
+            cur_sum -= nums[left]
+            left += 1
+
+        if cur_sum == target:
+            max_len = max(max_len, right - left + 1)
+
+    return n - max_len if max_len else -1
+
+
+# nums = [1,1,4,2,3]
+# x = 5
+
+# nums = [5,6,7,8,9]
+# x = 4
+
+nums = [3,2,20,1,1,3]
+x = 10
+
+print(minOperations(nums, x))
