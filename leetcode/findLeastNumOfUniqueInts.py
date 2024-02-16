8bcb9885e287c5662fa8cae4d2ac132222b3fd05
Fiewor <johnfiewor@gmail.com>
2024-02-16 21:20:43 +0100

    add approach with O(n) time-complexity

--- leetcode/findLeastNumOfUniqueInts.py
+++ leetcode/findLeastNumOfUniqueInts.py
@@ -5,6 +5,7 @@ import heapq
 from typing import List
 
 # sorting
+# Time-complexity: O(nlogn)
 def findLeastNumOfUniqueInts(arr: List[int], k: int) -> int:
     c = Counter(arr)
     counts = sorted(c.items(), key=lambda x:x[1])
@@ -18,6 +19,7 @@ def findLeastNumOfUniqueInts(arr: List[int], k: int) -> int:
     return no_of_unique_elements
 
 # min-heap
+# Time-complexity: O(nlogn)
 def findLeastNumOfUniqueInts(arr: List[int], k: int) -> int:
     freq = Counter(arr)
     heap = list(freq.values())
@@ -33,9 +35,32 @@ def findLeastNumOfUniqueInts(arr: List[int], k: int) -> int:
         
     return res
 
+# Frequency array (bucket sort) approach
+# Time-complexity: O(n)
+def findLeastNumOfUniqueInts(arr: List[int], k: int) -> int:
+    freq = Counter(arr)
+    res = len(freq)
+    freq_list = [0] * (len(arr)+1)
 
-arr = [24,119,157,446,251,117,22,168,374,373,323,311,441,213,120,412,200,236,328,24,164,104,331,32,19,223,89,114,152,82,456,381,355,343,157,245,443,368,229,49,82,16,373,142,240,125,8]
-k = 41
+    for val in freq.values():
+        freq_list[val] += 1
+
+    for i in range(1, len(freq_list)):
+        remove = freq_list[i]
+        
+        if k >= i * remove:
+            k -= i * remove
+            res -= remove
+
+        else:
+            remove = k // i
+            res -= remove
+            break
+
+    return res
+
+# arr = [24,119,157,446,251,117,22,168,374,373,323,311,441,213,120,412,200,236,328,24,164,104,331,32,19,223,89,114,152,82,456,381,355,343,157,245,443,368,229,49,82,16,373,142,240,125,8]
+# k = 41
 arr = [4,3,1,1,3,3,2]
 k = 3
 # arr = [2,4,1,8,3,5,1,3]
