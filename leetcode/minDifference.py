1b3ff48aeab80dccc73c9d48ab2ce97d1928f81a
Fiewor <johnfiewor@gmail.com>
2024-06-10 19:40:47 +0100

    add solution to google interview question

---
+++ leetcode/minDifference.py
@@ -0,0 +1,16 @@
+from typing import List
+
+def minDifference(nums: List[int]) -> int:
+    nums.sort()
+    n = len(nums)
+    if n <= 4:
+        return 0
+    return min(
+        nums[n-4] - nums[0],
+        nums[n-3] - nums[1],
+        nums[n-2] - nums[2],
+        nums[n-1] - nums[3],
+    )
+
+nums = [3,100,20]
+print(minDifference(nums))
